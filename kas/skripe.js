f3fe9383c494bf62734332879750a60699357027
sikumel-ux <sikumel@gmail.com>
2026-06-03 20:33:52 +0700

    Update skripe.js

--- kas/skripe.js
+++ kas/skripe.js
@@ -3,7 +3,7 @@
 // ==========================================================================
 
 // URL REST API Google Apps Script Integration Engine
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwrICr6f5znEhcq5q9Hz85ipIc-41pz0HyVCr56kEOGd40CTLk7StmQKhSQwNKBBln6/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw2O7sydQVyXZZhrsqAMhTABZkFYkL2x5L2x2exlc71Y6Qm-NPiUXYsSKzTsLVR_IJIRQ/exec";
 
 const daftarBulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
 let dbGlobal = { kas: [], pembayaran: [], anggota: [], sampah: [] };
